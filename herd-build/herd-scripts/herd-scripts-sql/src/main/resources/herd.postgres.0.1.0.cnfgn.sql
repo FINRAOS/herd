8cb366a18e55f08a50377f96df09e33e8733f825
Arthur Felde <afelde@gmail.com>
2016-07-06 11:45:43 -0400

    DM-8220: Release Herd OSS
    - Fix SQL errors found during CloudFormation testing

--- herd-build/herd-scripts/herd-scripts-sql/src/main/resources/herd.postgres.0.1.0.cnfgn.sql
+++ herd-build/herd-scripts/herd-scripts-sql/src/main/resources/herd.postgres.0.1.0.cnfgn.sql
@@ -14,17 +14,6 @@
 * limitations under the License.
 */
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
 ----------------------------
 -- Configure parameters   --
 ----------------------------
