6b3e7e883f279de4c72cd55fc4318eb81cadb7ea
Zhang, Jim <jzhang80@gmail.com>
2017-03-30 10:07:54 -0400

    DM-9610 check FORMAT_USAGE_CODE value back to previous value

--- herd-code/herd-dao/src/test/java/org/finra/herd/dao/AbstractDaoTest.java
+++ herd-code/herd-dao/src/test/java/org/finra/herd/dao/AbstractDaoTest.java
@@ -257,7 +257,7 @@ public abstract class AbstractDaoTest extends AbstractCoreTest
 
     public static final String FORMAT_FILE_TYPE_DESCRIPTION = "UT_Description of " + FORMAT_FILE_TYPE_CODE;
 
-    public static final String FORMAT_USAGE_CODE = "UT_Usage_" + RANDOM_SUFFIX;
+    public static final String FORMAT_USAGE_CODE = "UT_Usage" + RANDOM_SUFFIX;
 
     public static final String FORMAT_USAGE_CODE_2 = "UT_Usage_2" + RANDOM_SUFFIX;
 
