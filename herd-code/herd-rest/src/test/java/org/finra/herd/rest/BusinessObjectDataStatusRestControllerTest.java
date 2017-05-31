4fb71054072bd839768712325aa530b6048d440f
Zhang, Jim <jzhang80@gmail.com>
2017-05-31 19:36:48 -0400

    DM-9925 refactor unit test

--- herd-code/herd-rest/src/test/java/org/finra/herd/rest/BusinessObjectDataStatusRestControllerTest.java
+++ herd-code/herd-rest/src/test/java/org/finra/herd/rest/BusinessObjectDataStatusRestControllerTest.java
@@ -15,38 +15,67 @@
 */
 package org.finra.herd.rest;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import org.finra.herd.model.api.xml.BusinessObjectDataKey;
 import org.finra.herd.model.api.xml.BusinessObjectDataStatusInformation;
 import org.finra.herd.model.api.xml.BusinessObjectDataStatusUpdateRequest;
 import org.finra.herd.model.api.xml.BusinessObjectDataStatusUpdateResponse;
 import org.finra.herd.model.jpa.BusinessObjectDataEntity;
+import org.finra.herd.service.BusinessObjectDataStatusService;
+import org.finra.herd.service.NotificationEventService;
 
 /**
  * This class tests various functionality within the business object data status REST controller.
  */
 public class BusinessObjectDataStatusRestControllerTest extends AbstractRestTest
 {
+    @Mock
+    private BusinessObjectDataStatusService businessObjectDataStatusService;
+
+    @Mock
+    private NotificationEventService notificationEventService;
+
+    @InjectMocks
+    private BusinessObjectDataStatusRestController businessObjectDataStatusRestController;
+
+    @Before()
+    public void before()
+    {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     public void testGetBusinessObjectDataStatus()
     {
-        // Create and persist database entities required for testing.
-        businessObjectDataDaoTestHelper
-            .createBusinessObjectDataEntity(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                SUBPARTITION_VALUES, DATA_VERSION, true, BDATA_STATUS);
+        BusinessObjectDataKey businessObjectDataKey =
+            new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
+                DATA_VERSION);
 
+        BusinessObjectDataStatusInformation businessObjectDataStatusInformation = new BusinessObjectDataStatusInformation();
+        when(businessObjectDataStatusService.getBusinessObjectDataStatus(businessObjectDataKey, PARTITION_KEY)).thenReturn(businessObjectDataStatusInformation);
         // Get the business object data status information.
         BusinessObjectDataStatusInformation resultBusinessObjectDataStatusInformation = businessObjectDataStatusRestController
             .getBusinessObjectDataStatus(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, PARTITION_KEY, PARTITION_VALUE,
                 getDelimitedFieldValues(SUBPARTITION_VALUES), FORMAT_VERSION, DATA_VERSION);
 
+        // Verify the external calls.
+        verify(businessObjectDataStatusService).getBusinessObjectDataStatus(businessObjectDataKey, PARTITION_KEY);
+        verifyNoMoreInteractions(businessObjectDataStatusService);
         // Validate the returned object.
-        businessObjectDataServiceTestHelper.validateBusinessObjectDataStatusInformation(
-            new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
-                DATA_VERSION), BDATA_STATUS, resultBusinessObjectDataStatusInformation);
+        assertEquals(businessObjectDataStatusInformation, resultBusinessObjectDataStatusInformation);
     }
 
     @Test
@@ -58,79 +87,85 @@ public class BusinessObjectDataStatusRestControllerTest extends AbstractRestTest
         {
             // Build a list of subpartition values.
             List<String> subPartitionValues = SUBPARTITION_VALUES.subList(0, i);
+            BusinessObjectDataStatusInformation businessObjectDataStatusInformation = new BusinessObjectDataStatusInformation();
 
-            // Create and persist a business object data entity.
-            businessObjectDataDaoTestHelper
-                .createBusinessObjectDataEntity(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                    subPartitionValues, DATA_VERSION, true, BDATA_STATUS);
+            BusinessObjectDataKey businessObjectDataKey =
+                new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, null, PARTITION_VALUE, subPartitionValues, null);
+            when(businessObjectDataStatusService.getBusinessObjectDataStatus(businessObjectDataKey, BLANK_TEXT))
+                .thenReturn(businessObjectDataStatusInformation);
 
             // Get the business object data status information without specifying optional parameters.
             BusinessObjectDataStatusInformation resultBusinessObjectDataStatusInformation = businessObjectDataStatusRestController
                 .getBusinessObjectDataStatus(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, BLANK_TEXT, PARTITION_VALUE,
                     getDelimitedFieldValues(subPartitionValues), null, null);
 
-            // Validate the returned object.
-            businessObjectDataServiceTestHelper.validateBusinessObjectDataStatusInformation(
-                new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, subPartitionValues,
-                    DATA_VERSION), BDATA_STATUS, resultBusinessObjectDataStatusInformation);
+            // Verify the external calls.
+            verify(businessObjectDataStatusService).getBusinessObjectDataStatus(businessObjectDataKey, BLANK_TEXT);
+            assertEquals(businessObjectDataStatusInformation, resultBusinessObjectDataStatusInformation);
         }
+        verifyNoMoreInteractions(businessObjectDataStatusService);
     }
 
     @Test
     public void testGetBusinessObjectDataStatusMissingOptionalParametersPassedAsNulls()
     {
-        // Create and persist a business object data entity without sub-partition values.
-        businessObjectDataDaoTestHelper
-            .createBusinessObjectDataEntity(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                NO_SUBPARTITION_VALUES, DATA_VERSION, true, BDATA_STATUS);
+        BusinessObjectDataStatusInformation businessObjectDataStatusInformation = new BusinessObjectDataStatusInformation();
+
+        BusinessObjectDataKey businessObjectDataKey =
+            new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, null, PARTITION_VALUE, new ArrayList<>(), null);
+        when(businessObjectDataStatusService.getBusinessObjectDataStatus(businessObjectDataKey, null)).thenReturn(businessObjectDataStatusInformation);
+
 
         // Get the business object data status by passing null values for the partition key and the list of sub-partition values.
         BusinessObjectDataStatusInformation resultBusinessObjectDataStatusInformation = businessObjectDataStatusRestController
             .getBusinessObjectDataStatus(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, null, PARTITION_VALUE, null, null, null);
 
-        // Validate the returned object.
-        businessObjectDataServiceTestHelper.validateBusinessObjectDataStatusInformation(
-            new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, NO_SUBPARTITION_VALUES,
-                DATA_VERSION), BDATA_STATUS, resultBusinessObjectDataStatusInformation);
+        // Verify the external calls.
+        verify(businessObjectDataStatusService).getBusinessObjectDataStatus(businessObjectDataKey, null);
+        assertEquals(businessObjectDataStatusInformation, resultBusinessObjectDataStatusInformation);
     }
 
     @Test
     public void testUpdateBusinessObjectDataStatus()
     {
-        // Create and persist relative test entities.
-        businessObjectDataDaoTestHelper
-            .createBusinessObjectDataEntity(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                SUBPARTITION_VALUES, DATA_VERSION, true, BDATA_STATUS);
-        businessObjectDataStatusDaoTestHelper.createBusinessObjectDataStatusEntity(BDATA_STATUS_2);
+        BusinessObjectDataKey businessObjectDataKey =
+            new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
+                DATA_VERSION);
+        BusinessObjectDataStatusUpdateRequest request = new BusinessObjectDataStatusUpdateRequest(BDATA_STATUS_2);
+
+        BusinessObjectDataStatusUpdateResponse businessObjectDataStatusUpdateResponse = new BusinessObjectDataStatusUpdateResponse();
+        when(businessObjectDataStatusService.updateBusinessObjectDataStatus(businessObjectDataKey, request)).thenReturn(businessObjectDataStatusUpdateResponse);
 
         // Update the business object data status.
-        BusinessObjectDataStatusUpdateResponse response = businessObjectDataStatusRestController
+        BusinessObjectDataStatusUpdateResponse updatedResponse = businessObjectDataStatusRestController
             .updateBusinessObjectDataStatus(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                SUBPARTITION_VALUES.get(0), SUBPARTITION_VALUES.get(1), SUBPARTITION_VALUES.get(2), SUBPARTITION_VALUES.get(3), DATA_VERSION,
-                new BusinessObjectDataStatusUpdateRequest(BDATA_STATUS_2));
+                SUBPARTITION_VALUES.get(0), SUBPARTITION_VALUES.get(1), SUBPARTITION_VALUES.get(2), SUBPARTITION_VALUES.get(3), DATA_VERSION, request);
 
+
+        // Verify the external calls.
+        verify(businessObjectDataStatusService).updateBusinessObjectDataStatus(businessObjectDataKey, request);
+        verifyNoMoreInteractions(businessObjectDataStatusService);
         // Validate the returned object.
-        businessObjectDataServiceTestHelper.validateBusinessObjectDataStatusUpdateResponse(
-            new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
-                DATA_VERSION), BDATA_STATUS_2, BDATA_STATUS, response);
+        assertEquals(businessObjectDataStatusUpdateResponse, updatedResponse);
     }
 
     @Test
     public void testUpdateBusinessObjectDataStatusMissingOptionalParameters()
     {
-        // Create and persist a business object data status entity.
-        businessObjectDataStatusDaoTestHelper.createBusinessObjectDataStatusEntity(BDATA_STATUS_2);
-
         // Test if we can retrieve an attribute for the business object data with any allowed number of subpartition values (from 0 to MAX_SUBPARTITIONS).
         for (int i = 0; i <= BusinessObjectDataEntity.MAX_SUBPARTITIONS; i++)
         {
             // Build a list of subpartition values.
             List<String> subPartitionValues = SUBPARTITION_VALUES.subList(0, i);
 
-            // Create and persist a business object data entity.
-            businessObjectDataDaoTestHelper
-                .createBusinessObjectDataEntity(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                    subPartitionValues, DATA_VERSION, true, BDATA_STATUS);
+            BusinessObjectDataKey businessObjectDataKey =
+                new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, subPartitionValues,
+                    DATA_VERSION);
+            BusinessObjectDataStatusUpdateRequest request = new BusinessObjectDataStatusUpdateRequest(BDATA_STATUS_2);
+            BusinessObjectDataStatusUpdateResponse businessObjectDataStatusUpdateResponse = new BusinessObjectDataStatusUpdateResponse();
+
+            when(businessObjectDataStatusService.updateBusinessObjectDataStatus(businessObjectDataKey, request))
+                .thenReturn(businessObjectDataStatusUpdateResponse);
 
             // Update the business object data status using the relative endpoint.
             BusinessObjectDataStatusUpdateResponse response = null;
@@ -138,6 +173,7 @@ public class BusinessObjectDataStatusRestControllerTest extends AbstractRestTest
             switch (i)
             {
                 case 0:
+
                     response = businessObjectDataStatusRestController
                         .updateBusinessObjectDataStatus(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
                             DATA_VERSION, new BusinessObjectDataStatusUpdateRequest(BDATA_STATUS_2));
@@ -165,11 +201,9 @@ public class BusinessObjectDataStatusRestControllerTest extends AbstractRestTest
                             new BusinessObjectDataStatusUpdateRequest(BDATA_STATUS_2));
                     break;
             }
-
-            // Validate the returned object.
-            businessObjectDataServiceTestHelper.validateBusinessObjectDataStatusUpdateResponse(
-                new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, subPartitionValues,
-                    DATA_VERSION), BDATA_STATUS_2, BDATA_STATUS, response);
+            verify(businessObjectDataStatusService).updateBusinessObjectDataStatus(businessObjectDataKey, request);
+            assertEquals(businessObjectDataStatusUpdateResponse, response);
         }
+        verifyNoMoreInteractions(businessObjectDataStatusService);
     }
 }
--- herd-code/herd-rest/src/test/java/org/finra/herd/rest/BusinessObjectDataStorageFileRestControllerTest.java
+++ herd-code/herd-rest/src/test/java/org/finra/herd/rest/BusinessObjectDataStorageFileRestControllerTest.java
@@ -15,171 +15,55 @@
 */
 package org.finra.herd.rest;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import org.finra.herd.model.api.xml.BusinessObjectDataStorageFilesCreateRequest;
 import org.finra.herd.model.api.xml.BusinessObjectDataStorageFilesCreateResponse;
-import org.finra.herd.model.api.xml.SchemaColumn;
-import org.finra.herd.model.api.xml.StorageFile;
-import org.finra.herd.model.dto.S3FileTransferRequestParamsDto;
-import org.finra.herd.model.jpa.BusinessObjectDataEntity;
-import org.finra.herd.model.jpa.BusinessObjectDataStatusEntity;
-import org.finra.herd.model.jpa.BusinessObjectDefinitionEntity;
-import org.finra.herd.model.jpa.BusinessObjectFormatEntity;
-import org.finra.herd.model.jpa.DataProviderEntity;
-import org.finra.herd.model.jpa.FileTypeEntity;
-import org.finra.herd.model.jpa.NamespaceEntity;
-import org.finra.herd.model.jpa.StorageEntity;
-import org.finra.herd.model.jpa.StorageUnitEntity;
-import org.finra.herd.model.jpa.StorageUnitStatusEntity;
+import org.finra.herd.service.BusinessObjectDataStorageFileService;
 
 /**
  * This class tests CreateBusinessObjectDataStorageFiles functionality within the business object data storage file REST controller.
  */
 public class BusinessObjectDataStorageFileRestControllerTest extends AbstractRestTest
 {
-    private static final String FILE_PATH_1 = "file1";
+    @Mock
+    private BusinessObjectDataStorageFileService businessObjectDataStorageFileService;
 
-    private static final String FILE_PATH_2 = "file2";
+    @InjectMocks
+    private BusinessObjectDataStorageFileRestController businessObjectDataStorageFileRestController;
 
-    private static final String PARTITION_KEY_2 = "pk2_" + Math.random();
-
-    private static final String PARTITION_KEY_3 = "pk3_" + Math.random();
-
-    private static final String PARTITION_KEY_4 = "pk4_" + Math.random();
-
-    private static final String PARTITION_KEY_5 = "pk5_" + Math.random();
-
-    private static final String PARTITION_VALUE_2 = "pv2_" + Math.random();
-
-    private static final String PARTITION_VALUE_3 = "pv3_" + Math.random();
-
-    private static final String PARTITION_VALUE_4 = "pv4_" + Math.random();
-
-    private static final String PARTITION_VALUE_5 = "pv5_" + Math.random();
-
-    private static final List<String> SUB_PARTITION_VALUES = Arrays.asList(PARTITION_VALUE_2, PARTITION_VALUE_3, PARTITION_VALUE_4, PARTITION_VALUE_5);
-
-    private static final String testS3KeyPrefix =
-        getExpectedS3KeyPrefix(NAMESPACE, DATA_PROVIDER_NAME, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_KEY,
-            PARTITION_VALUE, null, null, DATA_VERSION);
-
-    private Path localTempPath;
-
-    /**
-     * Sets up the test environment.
-     */
-    @Before
-    public void setupEnv() throws IOException
+    @Before()
+    public void before()
     {
-        // Create a local temp directory.
-        localTempPath = Files.createTempDirectory(null);
+        MockitoAnnotations.initMocks(this);
     }
-
-    /**
-     * Cleans up the local temp directory and S3 test path that we are using.
-     */
-    @After
-    public void cleanEnv() throws IOException
-    {
-        // Clean up the local directory.
-        FileUtils.deleteDirectory(localTempPath.toFile());
-
-        // Clean up the destination S3 folder.
-        S3FileTransferRequestParamsDto s3FileTransferRequestParamsDto = s3DaoTestHelper.getTestS3FileTransferRequestParamsDto();
-        s3FileTransferRequestParamsDto.setS3KeyPrefix(testS3KeyPrefix);
-        s3Dao.deleteDirectory(s3FileTransferRequestParamsDto);
-    }
-
+    
     @Test
     public void testCreateBusinessObjectDataStorageFiles()
     {
-        createDataWithSubPartitions();
+        BusinessObjectDataStorageFilesCreateResponse response = new BusinessObjectDataStorageFilesCreateResponse();
 
         BusinessObjectDataStorageFilesCreateRequest request =
             new BusinessObjectDataStorageFilesCreateRequest(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                SUB_PARTITION_VALUES, DATA_VERSION, STORAGE_NAME, Arrays.asList(createFile(FILE_PATH_2, FILE_SIZE_1_KB, ROW_COUNT_1000)),
+                null, DATA_VERSION, STORAGE_NAME, null,
                 NO_DISCOVER_STORAGE_FILES);
 
-        BusinessObjectDataStorageFilesCreateResponse response = businessObjectDataStorageFileRestController.createBusinessObjectDataStorageFiles(request);
+        when(businessObjectDataStorageFileService.createBusinessObjectDataStorageFiles(request)).thenReturn(response);
+        BusinessObjectDataStorageFilesCreateResponse resultResponse = businessObjectDataStorageFileRestController.createBusinessObjectDataStorageFiles(request);
 
+        // Verify the external calls.
+        verify(businessObjectDataStorageFileService).createBusinessObjectDataStorageFiles(request);
+        verifyNoMoreInteractions(businessObjectDataStorageFileService);
         // Validate the returned object.
-        businessObjectDataServiceTestHelper
-            .validateBusinessObjectDataStorageFilesCreateResponse(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION,
-                PARTITION_VALUE, SUB_PARTITION_VALUES, DATA_VERSION, STORAGE_NAME, request.getStorageFiles(), response);
-    }
-
-    private StorageFile createFile(String filePath, Long size, Long rowCount)
-    {
-        StorageFile f = new StorageFile();
-        f.setFilePath(filePath);
-        f.setFileSizeBytes(size);
-        f.setRowCount(rowCount);
-        return f;
-    }
-
-    private void createDataWithSubPartitions()
-    {
-        NamespaceEntity namespaceEntity = namespaceDaoTestHelper.createNamespaceEntity(NAMESPACE);
-        DataProviderEntity dataProviderEntity = dataProviderDaoTestHelper.createDataProviderEntity(DATA_PROVIDER_NAME);
-        BusinessObjectDefinitionEntity businessObjectDefinitionEntity = businessObjectDefinitionDaoTestHelper
-            .createBusinessObjectDefinitionEntity(namespaceEntity, BDEF_NAME, dataProviderEntity, NO_BDEF_DESCRIPTION, NO_BDEF_DISPLAY_NAME, NO_ATTRIBUTES,
-                NO_SAMPLE_DATA_FILES);
-        FileTypeEntity fileTypeEntity = fileTypeDaoTestHelper.createFileTypeEntity(FORMAT_FILE_TYPE_CODE, FORMAT_DESCRIPTION);
-        List<SchemaColumn> schemaColumns = new ArrayList<>();
-        {
-            SchemaColumn schemaColumn = new SchemaColumn();
-            schemaColumn.setName(PARTITION_KEY);
-            schemaColumn.setType("STRING");
-            schemaColumns.add(schemaColumn);
-        }
-        {
-            SchemaColumn schemaColumn = new SchemaColumn();
-            schemaColumn.setName(PARTITION_KEY_2);
-            schemaColumn.setType("STRING");
-            schemaColumns.add(schemaColumn);
-        }
-        {
-            SchemaColumn schemaColumn = new SchemaColumn();
-            schemaColumn.setName(PARTITION_KEY_3);
-            schemaColumn.setType("STRING");
-            schemaColumns.add(schemaColumn);
-        }
-        {
-            SchemaColumn schemaColumn = new SchemaColumn();
-            schemaColumn.setName(PARTITION_KEY_4);
-            schemaColumn.setType("STRING");
-            schemaColumns.add(schemaColumn);
-        }
-        {
-            SchemaColumn schemaColumn = new SchemaColumn();
-            schemaColumn.setName(PARTITION_KEY_5);
-            schemaColumn.setType("STRING");
-            schemaColumns.add(schemaColumn);
-        }
-        BusinessObjectFormatEntity businessObjectFormatEntity = businessObjectFormatDaoTestHelper
-            .createBusinessObjectFormatEntity(businessObjectDefinitionEntity, FORMAT_USAGE_CODE, fileTypeEntity, FORMAT_VERSION, null, true, PARTITION_KEY,
-                null, NO_ATTRIBUTES, null, null, null, schemaColumns, null);
-        BusinessObjectDataStatusEntity businessObjectDataStatusEntity =
-            businessObjectDataStatusDaoTestHelper.createBusinessObjectDataStatusEntity(BDATA_STATUS, DESCRIPTION, BDATA_STATUS_PRE_REGISTRATION_FLAG_SET);
-        BusinessObjectDataEntity businessObjectDataEntity = businessObjectDataDaoTestHelper
-            .createBusinessObjectDataEntity(businessObjectFormatEntity, PARTITION_VALUE, SUB_PARTITION_VALUES, DATA_VERSION, true,
-                businessObjectDataStatusEntity.getCode());
-
-        StorageEntity storageEntity = storageDaoTestHelper.createStorageEntity(STORAGE_NAME);
-        StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
-            .createStorageUnitEntity(storageEntity, businessObjectDataEntity, StorageUnitStatusEntity.ENABLED, NO_STORAGE_DIRECTORY_PATH);
-
-        storageFileDaoTestHelper.createStorageFileEntity(storageUnitEntity, FILE_PATH_1, FILE_SIZE_1_KB, null);
+        assertEquals(response, resultResponse);
     }
 }
