be489d24edcbf812454553d0f0e0063d39a0cc57
k26475 <kenneth.steward@finra.org>
2017-10-27 18:39:23 -0400

    DM-10380 Updated Herd to use newest version of swagger-codegen

---
+++ herd-code/herd-sdk/src/tsconfig/angular/build.bat
@@ -0,0 +1,10 @@
+rd dist /s /q 
+rd build /s /q 
+
+"node_modules/.bin/ngc" -p tsconfig-build.json^
+ && "node_modules/.bin/rollup" build/angular-client.js --output.file dist/angular-client.js --output.format es --external @angular/core,@angular/http,@angular/common,rxjs/add/observable/throw,rxjs/add/operator/catch,rxjs/add/operator/map,tslib^
+ && "node_modules/.bin/ngc" -p tsconfig-build.es5.json^
+ && "node_modules/.bin/rollup" build/angular-client.js --output.file dist/angular-client.es5.js --output.format es --external @angular/core,@angular/http,@angular/common,rxjs/add/observable/throw,rxjs/add/operator/catch,rxjs/add/operator/map,tslib^
+ && "node_modules/.bin/ngc" -p tsconfig-build.umd.json^
+ && "node_modules/.bin/rollup" build/angular-client.js --output.file dist/angular-client.umd.js --output.format umd --external @angular/core,@angular/http,@angular/common,rxjs/add/observable/throw,rxjs/add/operator/catch,rxjs/add/operator/map,tslib^
+ && robocopy build dist /s /XF *.js
