a550f11a0b3800db10e9f4b852e78fa2ecf6f8d9
Aniruddha Das <aniruddhadas9@gmail.com>
2017-12-18 14:38:31 -0500

    DM-10417: Retrieve BData eligible for destruction in format for FINRA RDL process
    - Fix handling of business object data without sub-partitions

--- herd-code/herd-tools/herd-retention-expiration-exporter/src/main/java/org/finra/herd/tools/retention/exporter/ExporterController.java
+++ herd-code/herd-tools/herd-retention-expiration-exporter/src/main/java/org/finra/herd/tools/retention/exporter/ExporterController.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2015 herd contributors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 herd contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.finra.herd.tools.retention.exporter;
 
 import java.io.File;
@@ -131,12 +131,15 @@ public class ExporterController
 
             for (BusinessObjectData businessObjectData : businessObjectDataList)
             {
+                int subPartitionsCount = CollectionUtils.size(businessObjectData.getSubPartitionValues());
                 List<String> businessObjectDataRecords = Arrays.asList(businessObjectData.getNamespace(), businessObjectData.getBusinessObjectDefinitionName(),
                     businessObjectData.getBusinessObjectFormatUsage(), businessObjectData.getBusinessObjectFormatFileType(),
                     Integer.toString(businessObjectData.getBusinessObjectFormatVersion()), businessObjectData.getPartitionValue(),
-                    businessObjectData.getSubPartitionValues().get(0), businessObjectData.getSubPartitionValues().get(1),
-                    businessObjectData.getSubPartitionValues().get(2), businessObjectData.getSubPartitionValues().get(3),
-                    Integer.toString(businessObjectData.getVersion()), businessObjectDefinitionUdcUri);
+                    subPartitionsCount > 0 ? businessObjectData.getSubPartitionValues().get(0) : "",
+                    subPartitionsCount > 1 ? businessObjectData.getSubPartitionValues().get(1) : "",
+                    subPartitionsCount > 2 ? businessObjectData.getSubPartitionValues().get(2) : "",
+                    subPartitionsCount > 3 ? businessObjectData.getSubPartitionValues().get(3) : "", Integer.toString(businessObjectData.getVersion()),
+                    businessObjectDefinitionUdcUri);
                 writeLine(writer, businessObjectDataRecords);
             }
         }
