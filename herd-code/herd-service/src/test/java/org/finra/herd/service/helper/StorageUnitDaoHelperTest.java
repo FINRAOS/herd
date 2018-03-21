a40791f7dabe1752b2334ab602fb5f06e0fb56ab
Arthur Felde <afelde@gmail.com>
2018-03-21 12:04:15 -0400

    DM-10605: Receive messages for Storage Unit Status changes
    - Re-factor StorageUnitDaoHelper unit tests to use Mockito

--- herd-code/herd-service/src/test/java/org/finra/herd/service/helper/StorageUnitDaoHelperTest.java
+++ herd-code/herd-service/src/test/java/org/finra/herd/service/helper/StorageUnitDaoHelperTest.java
@@ -16,11 +16,18 @@
 package org.finra.herd.service.helper;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import org.finra.herd.dao.StorageUnitDao;
 import org.finra.herd.model.ObjectNotFoundException;
 import org.finra.herd.model.api.xml.BusinessObjectDataKey;
 import org.finra.herd.model.api.xml.BusinessObjectDataStorageUnitKey;
@@ -28,55 +35,147 @@ import org.finra.herd.model.jpa.BusinessObjectDataEntity;
 import org.finra.herd.model.jpa.StorageUnitEntity;
 import org.finra.herd.model.jpa.StorageUnitStatusEntity;
 import org.finra.herd.service.AbstractServiceTest;
+import org.finra.herd.service.MessageNotificationEventService;
 
 /**
  * This class tests functionality within the StorageUnitDaoHelper class.
  */
 public class StorageUnitDaoHelperTest extends AbstractServiceTest
 {
-    @Test
-    public void testGetStorageUnitEntity()
+    @Mock
+    private BusinessObjectDataHelper businessObjectDataHelper;
+
+    @Mock
+    private MessageNotificationEventService messageNotificationEventService;
+
+    @Mock
+    private StorageUnitDao storageUnitDao;
+
+    @InjectMocks
+    private StorageUnitDaoHelper storageUnitDaoHelper;
+
+    @Mock
+    private StorageUnitStatusDaoHelper storageUnitStatusDaoHelper;
+
+    @Before
+    public void before()
     {
-        // Create and persist test database entities.
-        StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
-            .createStorageUnitEntity(STORAGE_NAME, NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                NO_SUBPARTITION_VALUES, DATA_VERSION, true, BDATA_STATUS, STORAGE_UNIT_STATUS, NO_STORAGE_DIRECTORY_PATH);
-        BusinessObjectDataEntity businessObjectDataEntity = storageUnitEntity.getBusinessObjectData();
-
-        // Try to retrieve a non existing storage unit.
-        try
-        {
-            storageUnitDaoHelper.getStorageUnitEntity("I_DO_NOT_EXIST", businessObjectDataEntity);
-            fail("Should throw an ObjectNotFoundException.");
-        }
-        catch (ObjectNotFoundException e)
-        {
-            assertEquals(String.format("Could not find storage unit in \"I_DO_NOT_EXIST\" storage for the business object data {%s}.", businessObjectDataHelper
-                .businessObjectDataKeyToString(
-                    new BusinessObjectDataKey(NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                        NO_SUBPARTITION_VALUES, DATA_VERSION))), e.getMessage());
-        }
-
-        // Retrieve an existing storage unit entity.
-        StorageUnitEntity resultStorageUnitEntity = storageUnitDaoHelper.getStorageUnitEntity(STORAGE_NAME, businessObjectDataEntity);
-        assertNotNull(resultStorageUnitEntity);
-        assertEquals(storageUnitEntity.getId(), resultStorageUnitEntity.getId());
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testGetStorageUnitEntityByKey()
+    public void testGetStorageUnitEntityByBusinessObjectDataAndStorageName()
+    {
+        // Create and persist test database entities.
+        StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
+            .createStorageUnitEntity(STORAGE_NAME, BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
+                NO_SUBPARTITION_VALUES, DATA_VERSION, true, BDATA_STATUS, STORAGE_UNIT_STATUS, NO_STORAGE_DIRECTORY_PATH);
+
+        // Get the business object data entity.
+        BusinessObjectDataEntity businessObjectDataEntity = storageUnitEntity.getBusinessObjectData();
+
+        // Create a storage unit key.
+        BusinessObjectDataStorageUnitKey businessObjectDataStorageUnitKey =
+            new BusinessObjectDataStorageUnitKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
+                SUBPARTITION_VALUES, DATA_VERSION, STORAGE_NAME);
+
+        // Mock the external calls.
+        when(storageUnitDao.getStorageUnitByBusinessObjectDataAndStorageName(businessObjectDataEntity, STORAGE_NAME)).thenReturn(storageUnitEntity);
+
+        // Call the method under test.
+        StorageUnitEntity result = storageUnitDaoHelper.getStorageUnitEntity(STORAGE_NAME, businessObjectDataEntity);
+
+        // Verify the external calls.
+        verify(storageUnitDao).getStorageUnitByBusinessObjectDataAndStorageName(businessObjectDataEntity, STORAGE_NAME);
+        verifyNoMoreInteractionsHelper();
+
+        // Validate the results.
+        assertEquals(storageUnitEntity, result);
+    }
+
+    @Test
+    public void testGetStorageUnitEntityByBusinessObjectDataAndStorageNameStorageUnitNoExists()
     {
         // Create a business object data key.
         BusinessObjectDataKey businessObjectDataKey =
             new BusinessObjectDataKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
                 DATA_VERSION);
 
-        // Create a business object data storage unit key.
+        // Create and persist test database entities.
+        StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
+            .createStorageUnitEntity(STORAGE_NAME, BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
+                NO_SUBPARTITION_VALUES, DATA_VERSION, true, BDATA_STATUS, STORAGE_UNIT_STATUS, NO_STORAGE_DIRECTORY_PATH);
+
+        // Get the business object data entity.
+        BusinessObjectDataEntity businessObjectDataEntity = storageUnitEntity.getBusinessObjectData();
+
+        // Mock the external calls.
+        when(storageUnitDao.getStorageUnitByBusinessObjectDataAndStorageName(businessObjectDataEntity, STORAGE_NAME)).thenReturn(null);
+        when(businessObjectDataHelper.businessObjectDataEntityAltKeyToString(businessObjectDataEntity)).thenReturn(BUSINESS_OBJECT_DATA_KEY_AS_STRING);
+
+        // Try to call the method under test.
+        try
+        {
+            storageUnitDaoHelper.getStorageUnitEntity(STORAGE_NAME, businessObjectDataEntity);
+            fail();
+        }
+        catch (ObjectNotFoundException e)
+        {
+            assertEquals(String
+                    .format("Could not find storage unit in \"%s\" storage for the business object data {%s}.", STORAGE_NAME, BUSINESS_OBJECT_DATA_KEY_AS_STRING),
+                e.getMessage());
+        }
+
+        // Verify the external calls.
+        verify(storageUnitDao).getStorageUnitByBusinessObjectDataAndStorageName(businessObjectDataEntity, STORAGE_NAME);
+        verify(businessObjectDataHelper).businessObjectDataEntityAltKeyToString(businessObjectDataEntity);
+        verifyNoMoreInteractionsHelper();
+    }
+
+    @Test
+    public void testGetStorageUnitEntityByKey()
+    {
+        // Create a storage unit key.
         BusinessObjectDataStorageUnitKey businessObjectDataStorageUnitKey =
             new BusinessObjectDataStorageUnitKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
                 SUBPARTITION_VALUES, DATA_VERSION, STORAGE_NAME);
 
-        // Try to retrieve a non existing storage unit.
+        // Create and persist a storage unit entity.
+        StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
+            .createStorageUnitEntity(STORAGE_NAME, BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
+                SUBPARTITION_VALUES, DATA_VERSION, LATEST_VERSION_FLAG_SET, BDATA_STATUS, STORAGE_UNIT_STATUS, NO_STORAGE_DIRECTORY_PATH);
+
+        // Mock the external calls.
+        when(storageUnitDao.getStorageUnitByKey(businessObjectDataStorageUnitKey)).thenReturn(storageUnitEntity);
+
+        // Call the method under test.
+        StorageUnitEntity result = storageUnitDaoHelper.getStorageUnitEntityByKey(businessObjectDataStorageUnitKey);
+
+        // Verify the external calls.
+        verify(storageUnitDao).getStorageUnitByKey(businessObjectDataStorageUnitKey);
+        verifyNoMoreInteractionsHelper();
+
+        // Validate the results.
+        assertEquals(storageUnitEntity, result);
+    }
+
+    @Test
+    public void testGetStorageUnitEntityByKeyStorageUnitNoExists()
+    {
+        // Create a business object data key.
+        BusinessObjectDataKey businessObjectDataKey =
+            new BusinessObjectDataKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
+                DATA_VERSION);
+
+        // Create a storage unit key.
+        BusinessObjectDataStorageUnitKey businessObjectDataStorageUnitKey =
+            new BusinessObjectDataStorageUnitKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
+                SUBPARTITION_VALUES, DATA_VERSION, STORAGE_NAME);
+
+        // Mock the external calls.
+        when(storageUnitDao.getStorageUnitByKey(businessObjectDataStorageUnitKey)).thenReturn(null);
+
+        // Try to call the method under test.
         try
         {
             storageUnitDaoHelper.getStorageUnitEntityByKey(businessObjectDataStorageUnitKey);
@@ -85,42 +184,22 @@ public class StorageUnitDaoHelperTest extends AbstractServiceTest
         catch (ObjectNotFoundException e)
         {
             assertEquals(String.format("Business object data storage unit {%s, storageName: \"%s\"} doesn't exist.",
-                businessObjectDataHelper.businessObjectDataKeyToString(businessObjectDataKey), STORAGE_NAME), e.getMessage());
+                businessObjectDataServiceTestHelper.getExpectedBusinessObjectDataKeyAsString(businessObjectDataKey), STORAGE_NAME), e.getMessage());
         }
 
-        // Create and persist test database entities.
-        StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper.createStorageUnitEntity(businessObjectDataStorageUnitKey, STORAGE_UNIT_STATUS);
-
-        // Retrieve an existing storage unit entity by key.
-        assertEquals(storageUnitEntity, storageUnitDaoHelper.getStorageUnitEntityByKey(businessObjectDataStorageUnitKey));
-    }
-
-    @Test
-    public void testGetStorageUnitEntityByKeyNoSubPartitionValuesNoStorageUnitExists()
-    {
-        // Create a business object data key without sub-partition values.
-        BusinessObjectDataKey businessObjectDataKey =
-            new BusinessObjectDataKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                NO_SUBPARTITION_VALUES, DATA_VERSION);
-
-        // Try to retrieve a non existing storage unit.
-        try
-        {
-            storageUnitDaoHelper.getStorageUnitEntityByKey(
-                new BusinessObjectDataStorageUnitKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
-                    NO_SUBPARTITION_VALUES, DATA_VERSION, STORAGE_NAME));
-            fail();
-        }
-        catch (ObjectNotFoundException e)
-        {
-            assertEquals(String.format("Business object data storage unit {%s, storageName: \"%s\"} doesn't exist.",
-                businessObjectDataHelper.businessObjectDataKeyToString(businessObjectDataKey), STORAGE_NAME), e.getMessage());
-        }
+        // Verify the external calls.
+        verify(storageUnitDao).getStorageUnitByKey(businessObjectDataStorageUnitKey);
+        verifyNoMoreInteractionsHelper();
     }
 
     @Test
     public void testSetStorageUnitStatus()
     {
+        // Create a business object data key.
+        BusinessObjectDataKey businessObjectDataKey =
+            new BusinessObjectDataKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
+                DATA_VERSION);
+
         // Create and persist a storage unit entity.
         StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
             .createStorageUnitEntity(STORAGE_NAME, BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
@@ -129,28 +208,95 @@ public class StorageUnitDaoHelperTest extends AbstractServiceTest
         // Create and persist a storage status entity.
         StorageUnitStatusEntity storageUnitStatusEntity = storageUnitStatusDaoTestHelper.createStorageUnitStatusEntity(STORAGE_UNIT_STATUS_2);
 
-        // Update the storage unit status.
+        // Mock the external calls.
+        when(businessObjectDataHelper.getBusinessObjectDataKey(storageUnitEntity.getBusinessObjectData())).thenReturn(businessObjectDataKey);
+
+        // Call the method under test.
         storageUnitDaoHelper.setStorageUnitStatus(storageUnitEntity, storageUnitStatusEntity);
+
+        // Verify the external calls.
+        verify(businessObjectDataHelper).getBusinessObjectDataKey(storageUnitEntity.getBusinessObjectData());
+        verify(messageNotificationEventService)
+            .processStorageUnitStatusChangeNotificationEvent(businessObjectDataKey, STORAGE_NAME, STORAGE_UNIT_STATUS_2, NO_STORAGE_UNIT_STATUS);
+        verifyNoMoreInteractionsHelper();
 
         // Validate the results.
         assertEquals(STORAGE_UNIT_STATUS_2, storageUnitEntity.getStatus().getCode());
     }
 
     @Test
-    public void testUpdateStorageUnitStatus()
+    public void testUpdateStorageUnitStatusNewStatusPassedAsEntity()
     {
+        // Create a business object data key.
+        BusinessObjectDataKey businessObjectDataKey =
+            new BusinessObjectDataKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
+                DATA_VERSION);
+
         // Create and persist a storage unit entity.
         StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
             .createStorageUnitEntity(STORAGE_NAME, BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
                 SUBPARTITION_VALUES, DATA_VERSION, LATEST_VERSION_FLAG_SET, BDATA_STATUS, STORAGE_UNIT_STATUS, NO_STORAGE_DIRECTORY_PATH);
 
         // Create and persist a storage status entity.
-        storageUnitStatusDaoTestHelper.createStorageUnitStatusEntity(STORAGE_UNIT_STATUS_2);
+        StorageUnitStatusEntity storageUnitStatusEntity = storageUnitStatusDaoTestHelper.createStorageUnitStatusEntity(STORAGE_UNIT_STATUS_2);
 
-        // Update the storage unit status.
-        storageUnitDaoHelper.updateStorageUnitStatus(storageUnitEntity, STORAGE_UNIT_STATUS_2, REASON);
+        // Mock the external calls.
+        when(businessObjectDataHelper.getBusinessObjectDataKey(storageUnitEntity.getBusinessObjectData())).thenReturn(businessObjectDataKey);
+
+        // Call the method under test.
+        storageUnitDaoHelper.updateStorageUnitStatus(storageUnitEntity, storageUnitStatusEntity, STORAGE_UNIT_STATUS_2);
+
+        // Verify the external calls.
+        verify(storageUnitDao).saveAndRefresh(storageUnitEntity);
+        verify(businessObjectDataHelper).getBusinessObjectDataKey(storageUnitEntity.getBusinessObjectData());
+        verify(messageNotificationEventService)
+            .processStorageUnitStatusChangeNotificationEvent(businessObjectDataKey, STORAGE_NAME, STORAGE_UNIT_STATUS_2, STORAGE_UNIT_STATUS);
+        verifyNoMoreInteractionsHelper();
 
         // Validate the results.
         assertEquals(STORAGE_UNIT_STATUS_2, storageUnitEntity.getStatus().getCode());
+    }
+
+    @Test
+    public void testUpdateStorageUnitStatusNewStatusPassedAsString()
+    {
+        // Create a business object data key.
+        BusinessObjectDataKey businessObjectDataKey =
+            new BusinessObjectDataKey(BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE, SUBPARTITION_VALUES,
+                DATA_VERSION);
+
+        // Create and persist a storage unit entity.
+        StorageUnitEntity storageUnitEntity = storageUnitDaoTestHelper
+            .createStorageUnitEntity(STORAGE_NAME, BDEF_NAMESPACE, BDEF_NAME, FORMAT_USAGE_CODE, FORMAT_FILE_TYPE_CODE, FORMAT_VERSION, PARTITION_VALUE,
+                SUBPARTITION_VALUES, DATA_VERSION, LATEST_VERSION_FLAG_SET, BDATA_STATUS, STORAGE_UNIT_STATUS, NO_STORAGE_DIRECTORY_PATH);
+
+        // Create and persist a storage status entity.
+        StorageUnitStatusEntity storageUnitStatusEntity = storageUnitStatusDaoTestHelper.createStorageUnitStatusEntity(STORAGE_UNIT_STATUS_2);
+
+        // Mock the external calls.
+        when(storageUnitStatusDaoHelper.getStorageUnitStatusEntity(STORAGE_UNIT_STATUS_2)).thenReturn(storageUnitStatusEntity);
+        when(businessObjectDataHelper.getBusinessObjectDataKey(storageUnitEntity.getBusinessObjectData())).thenReturn(businessObjectDataKey);
+
+        // Call the method under test.
+        storageUnitDaoHelper.updateStorageUnitStatus(storageUnitEntity, STORAGE_UNIT_STATUS_2, STORAGE_UNIT_STATUS_2);
+
+        // Verify the external calls.
+        verify(storageUnitStatusDaoHelper).getStorageUnitStatusEntity(STORAGE_UNIT_STATUS_2);
+        verify(storageUnitDao).saveAndRefresh(storageUnitEntity);
+        verify(businessObjectDataHelper).getBusinessObjectDataKey(storageUnitEntity.getBusinessObjectData());
+        verify(messageNotificationEventService)
+            .processStorageUnitStatusChangeNotificationEvent(businessObjectDataKey, STORAGE_NAME, STORAGE_UNIT_STATUS_2, STORAGE_UNIT_STATUS);
+        verifyNoMoreInteractionsHelper();
+
+        // Validate the results.
+        assertEquals(STORAGE_UNIT_STATUS_2, storageUnitEntity.getStatus().getCode());
+    }
+
+    /**
+     * Checks if any of the mocks has any interaction.
+     */
+    private void verifyNoMoreInteractionsHelper()
+    {
+        verifyNoMoreInteractions(businessObjectDataHelper, messageNotificationEventService, storageUnitDao, storageUnitStatusDaoHelper);
     }
 }
