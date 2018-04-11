985cc81037c4dc0907c54a931e247bf8af6ba62e
Arthur Felde <afelde@gmail.com>
2018-04-11 10:39:13 -0400

    DM-10634: Retrieve updated Relational Table schema when registered table has changed
    - Add an empty system job to update relational table schema

---
+++ herd-code/herd-service/src/main/java/org/finra/herd/service/systemjobs/RelationalTableSchemaUpdateSystemJob.java
@@ -0,0 +1,71 @@
+/*
+* Copyright 2015 herd contributors
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.finra.herd.service.systemjobs;
+
+import java.util.List;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import org.finra.herd.model.api.xml.Parameter;
+import org.finra.herd.model.dto.ConfigurationValue;
+
+/**
+ * The system job that updates schema for all relational tables registered in the system.
+ */
+@Component(RelationalTableSchemaUpdateSystemJob.JOB_NAME)
+@DisallowConcurrentExecution
+public class RelationalTableSchemaUpdateSystemJob extends AbstractSystemJob
+{
+    public static final String JOB_NAME = "relationalTableSchemaUpdate";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RelationalTableSchemaUpdateSystemJob.class);
+
+    @Override
+    public String getCronExpression()
+    {
+        return configurationHelper.getProperty(ConfigurationValue.RELATIONAL_TABLE_SCHEMA_UPDATE_JOB_CRON_EXPRESSION);
+    }
+
+    @Override
+    public JobDataMap getJobDataMap()
+    {
+        return getJobDataMapWithoutParameters();
+    }
+
+    @Override
+    public void validateParameters(List<Parameter> parameters)
+    {
+        // This system job accepts no parameters.
+        Assert.isTrue(org.springframework.util.CollectionUtils.isEmpty(parameters), String.format("\"%s\" system job does not except parameters.", JOB_NAME));
+    }
+
+    @Override
+    protected void executeInternal(JobExecutionContext context) throws JobExecutionException
+    {
+        // Log that the system job is started.
+        LOGGER.info("Started system job. systemJobName=\"{}\"", JOB_NAME);
+
+        // Log that the system job is ended.
+        LOGGER.info("Completed system job. systemJobName=\"{}\"", JOB_NAME);
+    }
+}
