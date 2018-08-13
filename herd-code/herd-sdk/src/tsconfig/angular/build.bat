1d023e3d0c9a357c5ce0474c43a0b7172cfd74d6
Aniruddha Das <aniruddhadas9@gmail.com>
2018-08-13 13:04:21 -0400

    DM-10691: FactoryCQ - UI - Migrate from Http to HttpClient
    - upgrade to angular to 6.1.1 and rxjs to 6.2.2. for herd angular client.

--- herd-code/herd-sdk/src/tsconfig/angular/build.bat
+++ herd-code/herd-sdk/src/tsconfig/angular/build.bat
@@ -2,8 +2,8 @@ rd dist /s /q
 rd build /s /q
 
 "node_modules/.bin/ngc" -p tsconfig-build.json^
- && "node_modules/.bin/rollup" build/angular-client.js --output.file dist/angular-client.js --output.format es --external @angular/core,@angular/common,rxjs/add/observable/throw,rxjs/add/operator/catch,rxjs/add/operator/map,tslib^
+ && "node_modules/.bin/rollup" build/angular-client.js --output.file dist/angular-client.js --output.format es --external @angular/core,@angular/common,tslib^
  && "node_modules/.bin/ngc" -p tsconfig-build.es5.json^
- && "node_modules/.bin/rollup" build/angular-client.js --output.file dist/angular-client.es5.js --output.format es --external @angular/core,@angular/common,rxjs/add/observable/throw,rxjs/add/operator/catch,rxjs/add/operator/map,tslib^
- && "node_modules/.bin/rollup" build/angular-client.js --name "clientBundle" --output.file dist/angular-client.umd.js --output.format umd --external @angular/core,@angular/common,rxjs/add/observable/throw,rxjs/add/operator/catch,rxjs/add/operator/map,tslib^
+ && "node_modules/.bin/rollup" build/angular-client.js --output.file dist/angular-client.es5.js --output.format es --external @angular/core,@angular/common,tslib^
+ && "node_modules/.bin/rollup" build/angular-client.js --name "clientBundle" --output.file dist/angular-client.umd.js --output.format umd --external @angular/core,@angular/common,tslib^
  && robocopy build dist /s /XF *.js
