18f9aef3d6f3c1111f0359d7f2e56c5ba5f612d2
Charles Sun <Charles.Sun@finra.org>
2019-11-12 11:49:42 -0500

    DM-12174: Manage Content Loader as Herd/DM tool - Separate gui from console application

--- herd-code/herd-tools/herd-content-loader/herdcl/application.py
+++ herd-code/herd-tools/herd-content-loader/herdcl/application.py
@@ -14,254 +14,58 @@
   limitations under the License.
 """
 # Standard library imports
-import os, sys, argparse, configparser, traceback, base64
-
-import tkinter as tk
-from tkinter import font, ttk, scrolledtext, filedialog
+import argparse, base64
 
 # Local imports
 from herdcl import logger, otags
 
 LOGGER = logger.get_logger(__name__)
-ALL = tk.N + tk.S + tk.E + tk.W
-ACTIONS = ["Tags", "Objects", "Columns", "Samples", "BDef Description"]
-ENVS = ["DEV", "DEV-INT", "QA", "QA-INT", "QA-STRESS", "PROD", "PROD-CT"]
-
 
 ################################################################################
-class Application(tk.Frame):
+class Application:
     """
-     The GUI Application class.
+     The application class. Main class
     """
-
-    names = None
-    fileName = ""
-    env = None
-    textPad = None
-    wb = None
-    gui_enabled = True
-
-    def __init__(self, master=None):
-        """
-        The init method for the Application class.
-
-        :param master: the parent widget
-        """
-
-        self.env_name = ENVS[0]
-        self.action = ACTIONS[0]
-
-        tk.Frame.__init__(self, master)
-        tk.font.nametofont("TkDefaultFont").configure(size=10)
-        tk.font.nametofont("TkTextFont").configure(size=11)
-        tk.font.nametofont("TkFixedFont").configure(size=11)
-
-        self.option_add('*Dialog.msg.font', 'Helvetica 14')
-
-        self.username = tk.StringVar()
-        self.userpwd = tk.StringVar()
-        self.delete = tk.BooleanVar()
-        self.grid(sticky=ALL)
-
-        self.act = {0: self.test_api,
-                    1: self.test_api,
-                    2: self.test_api,
-                    3: self.test_api,
-                    4: self.test_api}
-        # TODO change act object
-
-        self.path, self.config = self.load_config()
-        self.load_console_vars()
-        self.create_widgets()
-
-    ############################################################################
-    @staticmethod
-    def load_config():
-        """
-        Load configuration file
-
-        :return: current working directory and configparser
-        """
-
-        LOGGER.debug(sys.argv)
-        if os.name == 'nt':
-            path = os.getcwd()
-        elif '/' in sys.argv[0]:
-            path = '/'.join(sys.argv[0].split('/')[:-1])
-        else:
-            path = os.getcwd()
-        LOGGER.debug('Current working directory: {}'.format(path))
-
-        # Get config file
-        config_file = path + "/loader.cfg"
-        LOGGER.debug('Checking for loader config: {}'.format(config_file))
-        if not os.path.exists(config_file):
-            message = "No config file found"
-            LOGGER.error(message)
-            raise Exception(message)
-        config = configparser.ConfigParser()
-        config.read(config_file)
-
-        return path, config
-
-    ############################################################################
-    def create_widgets(self):
-        """
-        Create forms on GUI
-        """
-
-        top = self.winfo_toplevel()
-        top.rowconfigure(0, weight=1)
-        top.columnconfigure(0, weight=1)
-
-        self.rowconfigure(2, weight=1)
-        self.columnconfigure(2, weight=1)
-
-        ######## row 0
-
-        user_frame = tk.ttk.Labelframe(self, text="UserId and Password")
-        user_frame.grid(row=0, column=0, columnspan=2, sticky=ALL)
-
-        username_entry = tk.ttk.Entry(user_frame, width=22, textvariable=self.username)
-        username_entry.grid(row=0, column=0, pady=5, padx=5, sticky=ALL)
-        userpwd_entry = tk.ttk.Entry(user_frame, width=22, show="*", textvariable=self.userpwd)
-        userpwd_entry.grid(row=0, column=1, pady=5, padx=5, sticky=ALL)
-
-        ######## row 1
-
-        env = tk.ttk.Labelframe(self, text='Environment')
-        self.env = tk.ttk.Combobox(env, values=ENVS, state='readonly', width=11)
-        self.env.bind("<<ComboboxSelected>>", self.select_env)
-        self.env.current(0)  # set selection
-        self.env.grid(row=0, pady=5, padx=5, sticky=ALL)
-        env.grid(row=1, column=0, sticky=ALL)
-
-        names = tk.ttk.Labelframe(self, text='Action')
-        self.names = tk.ttk.Combobox(names, values=ACTIONS, state='readonly', width=11)
-        self.names.bind("<<ComboboxSelected>>", self.select_action)
-        self.names.current(0)  # set selection
-        self.names.grid(row=0, pady=5, padx=5, sticky=ALL)
-        names.grid(row=1, column=1, sticky=ALL)
-
-        runs = tk.ttk.Labelframe(self, text='Go')
-        lb = tk.ttk.Button(runs, text="Run", command=self.run)
-        lb.grid(row=0, pady=5, padx=5, sticky=ALL)
-        runs.grid(row=1, column=3)  # , sticky=S)
-
-        ######## row 2
-
-        self.textPad = tk.scrolledtext.ScrolledText(self,
-                                                    inactiveselectbackground="grey")
-        self.textPad.grid(row=2, column=0, columnspan=4, sticky=ALL)
-        self.textPad.tag_configure("search", background="green")
-        self.textPad.tag_configure("error", foreground="red")
-        self.textPad.bind_class("Text", "<Control-a>", lambda event: event.widget.tag_add("sel", "1.0", "end"))
-
-    ############################################################################
-    def line(self, *args):
-        """
-        Outputs text and newline to GUI
-        """
-        output = ' '.join(str(a) for a in args)
-
-        if not output:
-            output = ''
-        else:
-            output = str(output)
-        self.show(output + "\n")
-
-    ############################################################################
-    def show(self, *args):
-        """
-        Outputs text to GUI
-        """
-        output = ' '.join(str(a) for a in args)
-
-        if not output:
-            output = '\n'
-        else:
-            output = str(output)
-
-        if "ERROR" in output:
-            self.textPad.insert(tk.END, output, "error")
-        else:
-            self.textPad.insert(tk.END, output)
-        self.textPad.see(tk.END)
-        self.textPad.update()
-
-    ############################################################################
-    def select_env(self, *args):
-        """
-        Gets env when user clicks on env dropdown
-        """
-        self.env_name = str(self.env.get())
-
-    ############################################################################
-    def select_action(self, *args):
-        """
-        Gets action when user clicks on action dropdown
-        """
-        self.action = str(self.names.get())
+    def __init__(self):
+        self.gui_enabled = False
+        self.controller = otags.Controller()
+        self.path, self.config = self.controller.load_config()
+        self.env = self.config.get('console', 'env')
+        self.action = self.config.get('console', 'action')
 
     ############################################################################
     def run(self):
         """
-        Runs program when user clicks Run button
+        Runs program by loading credentials and making call to controller
         """
-        self.textPad.delete('1.0', tk.END)
-
         creds = {
-            'url': self.config.get('url', self.env_name),
-            'userName': self.username.get(),
-            'userPwd': self.userpwd.get()
+            'url': self.config.get('url', self.env),
+            'userName': self.config.get('credentials', 'userName'),
+            'userPwd': base64.b64decode(self.config.get('credentials', 'userPwd')).decode('utf-8')
         }
-        otags.setup_config(creds)
+        self.controller.setup_config(creds)
 
-        try:
-            self.act[ACTIONS.index(self.action)]()
-        except Exception:
-            print(traceback.format_exc())
-
-        # TODO Run Summary
-        self.display("\n-- RUN COMPLETED ---")
-
-    ############################################################################
-    def test_api(self):
-        resp = otags.get_build_info()
-        self.display(resp)
-
-    ############################################################################
-    def display(self, resp):
+        resp = self.controller.run_action(str.lower(self.action))
         LOGGER.info(resp)
-        if self.gui_enabled:
-            self.line(resp)
 
-    ############################################################################
-    def get_config_value(self, section, key):
-        try:
-            return self.config.get(section, key)
-        except configparser.NoOptionError:
-            LOGGER.warning('Key: {} not found in config file'.format(key))
-            return None
-
-    ############################################################################
-    def load_console_vars(self):
-        self.username.set(self.get_config_value('credentials', 'userName'))
-        self.userpwd.set(base64.b64decode(self.get_config_value('credentials', 'userPwd')).decode('utf-8'))
-
+        LOGGER.info("\n-- RUN COMPLETED ---")
 
 ############################################################################
 def main():
-    app = Application()
-
+    """
+     The main method. Checks if argument has been passed to determine console mode or gui mode
+    """
+    main_app = Application()
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--console", help="Command Line Mode", action="store_true")
     args = parser.parse_args()
     if args.console:
-        app.gui_enabled = False
         LOGGER.info('Command Line Mode')
-        app.run()
+        main_app.run()
     else:
+        main_app.gui_enabled = True
+        import gui
+        app = gui.MainUI()
         LOGGER.info('Starting App')
         app.master.title('Herd Content Loader  v.20191029')
         app.mainloop()
--- herd-code/herd-tools/herd-content-loader/herdcl/otags.py
+++ herd-code/herd-tools/herd-content-loader/herdcl/otags.py
@@ -13,6 +13,9 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 """
+# Standard library imports
+import os, sys, configparser
+
 # Herd imports
 import herdsdk
 from herdsdk.rest import ApiException
@@ -22,39 +25,105 @@ from herdcl import logger
 
 LOGGER = logger.get_logger(__name__)
 
-# Configure HTTP basic authorization: basicAuthentication
-configuration = herdsdk.Configuration()
 
+################################################################################
+class Controller:
+    """
+     The controller class. Makes calls to herdsdk
+    """
+    def __init__(self):
+        # TODO attach methods
+        self.acts = {
+            'tags': self.get_build_info,
+            'objects': self.get_build_info,
+            'columns': self.get_build_info,
+            'samples': self.get_build_info,
+            'bdef description': self.get_build_info,
+        }
+        self.actions = ['Tags', 'Objects', 'Columns', 'Samples', 'BDef Description']
+        self.envs = ['DEV', 'DEV-INT', 'QA', 'QA-INT', 'QA-STRESS', 'PROD', 'PROD-CT']
 
-def setup_config(creds):
-    configuration.host = creds['url']
-    configuration.username = creds['userName']
-    configuration.password = creds['userPwd']
+        # Configure HTTP basic authorization: basicAuthentication
+        self.configuration = herdsdk.Configuration()
 
+    ############################################################################
+    def load_config(self):
+        """
+        Load configuration file
 
-def get_build_info():
-    # create an instance of the API class
-    api_instance = herdsdk.ApplicationApi(herdsdk.ApiClient(configuration))
+        :return: current working directory and configparser
+        """
+        LOGGER.debug(sys.argv)
+        if os.name == 'nt':
+            path = os.getcwd()
+        elif '/' in sys.argv[0]:
+            path = '/'.join(sys.argv[0].split('/')[:-1])
+        else:
+            path = os.getcwd()
+        LOGGER.debug('Current working directory: {}'.format(path))
 
-    try:
-        # Gets the build information
-        api_response = api_instance.application_get_build_info()
-        return api_response
-    except ApiException as e:
-        print("Exception when calling ApplicationApi->application_get_build_info: %s\n" % e)
+        # Get config file
+        config_file = path + "/loader.cfg"
+        LOGGER.debug('Checking for loader config: {}'.format(config_file))
+        if not os.path.exists(config_file):
+            message = "No config file found"
+            LOGGER.error(message)
+            raise Exception(message)
+        config = configparser.ConfigParser()
+        config.read(config_file)
 
+        return path, config
 
-def test_method():
-    # create an instance of the API class
-    api_instance = herdsdk.BusinessObjectDefinitionSubjectMatterExpertApi(herdsdk.ApiClient(configuration))
-    namespace = 'eFOCUS'  # str | the namespace code
-    business_object_definition_name = 'custody_flng_dtl_SEC'  # str | the business object definition name
+    ############################################################################
+    def setup_config(self, creds):
+        """
+        Setup url and credentials
 
-    try:
-        api_response = api_instance. \
-            business_object_definition_subject_matter_expert_get_business_object_definition_subject_matter_experts_by_business_object_definition(
-            namespace, business_object_definition_name)
-        return api_response.business_object_definition_subject_matter_expert_keys
-    except ApiException as e:
-        print(
-            "Exception when calling BusinessObjectDefinitionSubjectMatterExpertApi->business_object_definition_subject_matter_expert_get_business_object_definition_subject_matter_experts_by_business_object_definition: %s\n" % e)
+        """
+        self.configuration.host = creds['url']
+        self.configuration.username = creds['userName']
+        self.configuration.password = creds['userPwd']
+
+    ############################################################################
+    def run_action(self, key):
+        """
+        Runs a particular method. Returns result
+
+        :return: response from herdsdk call
+
+        """
+        return self.acts[key]()
+
+    ############################################################################
+    def get_build_info(self):
+        """
+        Gets the build information for the Data Management deployed code.
+
+        :return: response from herdsdk call
+
+        """
+        # create an instance of the API class
+        api_instance = herdsdk.ApplicationApi(herdsdk.ApiClient(self.configuration))
+
+        try:
+            # Gets the build information
+            api_response = api_instance.application_get_build_info()
+            return api_response
+        except ApiException as e:
+            print("Exception when calling ApplicationApi->application_get_build_info: %s\n" % e)
+
+    ############################################################################
+    def test_method(self):
+        # create an instance of the API class
+        api_instance = herdsdk.BusinessObjectDefinitionSubjectMatterExpertApi(herdsdk.ApiClient(self.configuration))
+        namespace = 'eFOCUS'  # str | the namespace code
+        business_object_definition_name = 'custody_flng_dtl_SEC'  # str | the business object definition name
+
+        try:
+            api_response = api_instance. \
+                business_object_definition_subject_matter_expert_get_business_object_definition_subject_matter_experts_by_business_object_definition(
+                namespace, business_object_definition_name)
+            return api_response.business_object_definition_subject_matter_expert_keys
+        except ApiException as e:
+            print(
+                "Exception when calling BusinessObjectDefinitionSubjectMatterExpertApi->business_object_definition_subject_matter_expert_get_business_object_definition_subject_matter_experts_by_business_object_definition: %s\n" % e)
