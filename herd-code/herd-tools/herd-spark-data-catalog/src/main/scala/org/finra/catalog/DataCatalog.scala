b510fe01bab28a288da5dcea4e141574e3d5f29b
Sweta Bansal <Sweta.Bansal@finra.org>
2020-02-12 15:35:28 -0500

    not removing escape character code

--- herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
+++ herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
@@ -797,7 +797,14 @@ class DataCatalog(val spark: SparkSession, host: String) extends Serializable {
       case None => delimiter = None
     }
 
-    escapeCharacter = actualEscapeCharacter
+    actualEscapeCharacter match
+    {
+      case Some(x) =>
+        if (x equalsIgnoreCase "\\") escapeCharacter = Option("\\")
+        else if (x contains "\\") escapeCharacter = Option(x.replace("\\u", "\\").replace("\\", "").toInt.toChar.toString)
+        else escapeCharacter = Option(x)
+      case None => escapeCharacter = None
+    }
 
     var parseOptions = scala.collection.mutable.Map(
       "delimiter" -> delimiter.orNull,
