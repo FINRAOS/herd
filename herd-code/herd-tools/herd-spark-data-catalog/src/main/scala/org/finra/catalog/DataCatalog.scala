dc6383456f1a77dfae58959bb925118e545ba343
Sweta Bansal <Sweta.Bansal@finra.org>
2020-02-12 14:43:15 -0500

    removed commented code

--- herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
+++ herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
@@ -797,15 +797,6 @@ class DataCatalog(val spark: SparkSession, host: String) extends Serializable {
       case None => delimiter = None
     }
 
-    /*actualEscapeCharacter match
-    {
-      case Some(x) =>
-        if (x equalsIgnoreCase "\\") escapeCharacter = Option("\\")
-        else if (x contains "\\") escapeCharacter = Option(x.replace("\\u", "\\").replace("\\", "").toInt.toChar.toString)
-        else escapeCharacter = Option(x)
-      case None => escapeCharacter = None
-    }*/
-
     escapeCharacter = actualEscapeCharacter
 
     var parseOptions = scala.collection.mutable.Map(
