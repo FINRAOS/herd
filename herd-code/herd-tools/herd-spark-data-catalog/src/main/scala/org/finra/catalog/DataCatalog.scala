1043dcc56b7d82ca9f60069bea39f25d57f9ff2a
Sweta Bansal <Sweta.Bansal@finra.org>
2020-02-13 15:27:25 -0500

    added 1 more case to exacpe char

--- herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
+++ herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
@@ -801,6 +801,7 @@ class DataCatalog(val spark: SparkSession, host: String) extends Serializable {
     {
       case Some(x) =>
         if (x equalsIgnoreCase "\\\\") escapeCharacter = Option("\\")
+        else if (x equalsIgnoreCase "\\") escapeCharacter = Option("\\")
         else if (x contains "\\") escapeCharacter = Option(x.replace("\\u", "\\").replace("\\", ""))
         else escapeCharacter = Option(x)
       case None => escapeCharacter = None
