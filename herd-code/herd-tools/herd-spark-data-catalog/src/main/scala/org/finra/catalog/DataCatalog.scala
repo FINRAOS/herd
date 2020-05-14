2f2d1f8d74f561a03a53dbc9fe8a986c5c5f60e1
Kumar Siddhartha <Kumar.Siddhartha@finra.org>
2020-05-14 14:43:02 -0400

    DM-12884: Herd/Spark - Add registerMLModel API into product code base
    - make mountPoint a required parameter to avoid a potential NPE

--- herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
+++ herd-code/herd-tools/herd-spark-data-catalog/src/main/scala/org/finra/catalog/DataCatalog.scala
@@ -1568,6 +1568,9 @@ class DataCatalog(val spark: SparkSession, host: String) extends Serializable {
                       isNewFormatVersion: Boolean = false,
                       formatVersion: Int = -1): String = {
 
+    // short-circuit execution if a mount-point is not specified
+    require(mountPoint.nonEmpty, "mountPoint is a required parameter, eg. \"/mnt/{bucket-name}\" or \"s3a://{bucket-name}\"")
+
     if (isNewFormatVersion) {
       // check if model exists and register new format version
       Try(herdApiWrapper.getHerdApi().getBusinessObjectByName(nameSpace, modelName)) match {
