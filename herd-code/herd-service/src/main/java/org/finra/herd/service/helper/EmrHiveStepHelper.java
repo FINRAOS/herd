aa420b794ae75133a3af351566fff4d4b9538a7e
Arthur Felde <afelde@gmail.com>
2020-08-04 09:17:29 -0400

    DM-13336: Launch EMR Clusters in another account with bootstrap action
    - Add AWS account specific staging bucket name to trusting account table

--- herd-code/herd-service/src/main/java/org/finra/herd/service/helper/EmrHiveStepHelper.java
+++ herd-code/herd-service/src/main/java/org/finra/herd/service/helper/EmrHiveStepHelper.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2015 herd contributors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 herd contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.finra.herd.service.helper;
 
 import java.util.ArrayList;
@@ -34,7 +34,7 @@ import org.finra.herd.model.api.xml.EmrHiveStepAddRequest;
 public class EmrHiveStepHelper extends EmrStepHelper
 {
     @Override
-    public Object buildResponseFromRequest(Object stepRequest)
+    public Object buildResponseFromRequest(Object stepRequest, String trustingAccountStagingBucketName)
     {
         EmrHiveStepAddRequest emrHiveStepAddRequest = (EmrHiveStepAddRequest) stepRequest;
         EmrHiveStep step = new EmrHiveStep();
@@ -44,7 +44,8 @@ public class EmrHiveStepHelper extends EmrStepHelper
         step.setEmrClusterName(emrHiveStepAddRequest.getEmrClusterName());
 
         step.setStepName(emrHiveStepAddRequest.getStepName().trim());
-        step.setScriptLocation(emrHiveStepAddRequest.getScriptLocation().trim().replaceAll(getS3ManagedReplaceString(), emrHelper.getS3StagingLocation()));
+        step.setScriptLocation(emrHiveStepAddRequest.getScriptLocation().trim()
+            .replaceAll(getS3ManagedReplaceString(), emrHelper.getS3StagingLocation(trustingAccountStagingBucketName)));
         // Add the script arguments
         if (!CollectionUtils.isEmpty(emrHiveStepAddRequest.getScriptArguments()))
         {
@@ -205,12 +206,12 @@ public class EmrHiveStepHelper extends EmrStepHelper
     @Override
     public String getRequestAccountId(Object stepRequest)
     {
-       return ((EmrHiveStepAddRequest) stepRequest).getAccountId();
+        return ((EmrHiveStepAddRequest) stepRequest).getAccountId();
     }
 
     @Override
     public void setRequestAccountId(Object stepRequest, String accountId)
     {
-        ((EmrHiveStepAddRequest) stepRequest).setAccountId(accountId);  
+        ((EmrHiveStepAddRequest) stepRequest).setAccountId(accountId);
     }
 }
