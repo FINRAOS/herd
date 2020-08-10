e86f75c030a9127f740e618972e4e7c42d47be7d
Kumar Siddhartha <Kumar.Siddhartha@finra.org>
2020-08-10 15:24:57 -0400

    Revert "DM-13327: Search for BData with timestamps in Registration Date filter"
    This reverts commit 5c5d2202

--- herd-code/herd-dao/src/main/java/org/finra/herd/dao/impl/BusinessObjectDataDaoImpl.java
+++ herd-code/herd-dao/src/main/java/org/finra/herd/dao/impl/BusinessObjectDataDaoImpl.java
@@ -1090,19 +1090,8 @@ public class BusinessObjectDataDaoImpl extends AbstractHerdDao implements Busine
         // Apply predicate for registration end timestamp
         if (registrationDateRangeFilter.getEndRegistrationDate() != null)
         {
-            Date endDate;
-
-            // Determine if registration end date contains a time portion; if not, assume end of day.
-            if (!HerdDateUtils.containsTimePortion(registrationDateRangeFilter.getEndRegistrationDate()))
-            {
-                endDate = DateUtils.addDays(HerdDateUtils.convertToTimestamp(registrationDateRangeFilter.getEndRegistrationDate()), 1);
-            }
-            else
-            {
-                endDate = HerdDateUtils.convertToTimestamp(registrationDateRangeFilter.getEndRegistrationDate());
-            }
-
-            predicate = builder.and(predicate, builder.lessThan(businessObjectDataEntity.get(BusinessObjectDataEntity_.createdOn), endDate));
+            predicate = builder.and(predicate, builder.lessThan(businessObjectDataEntity.get(BusinessObjectDataEntity_.createdOn),
+                HerdDateUtils.convertToTimestamp(registrationDateRangeFilter.getEndRegistrationDate())));
         }
 
         return predicate;
