89cd335e7ff8261f3b0bca72d7c6625a1cac94aa
Sweta Bansal <Sweta.Bansal@finra.org>
2020-11-17 15:08:37 -0500

    DM-13806 - Improve error handling in DataCatalog - Update if block

--- herd-code/herd-tools/herd-spark-data-source/src/main/scala/org/apache/spark/sql/herd/DefaultSource.scala
+++ herd-code/herd-tools/herd-spark-data-source/src/main/scala/org/apache/spark/sql/herd/DefaultSource.scala
@@ -343,10 +343,12 @@ class DefaultSource(apiClientFactory: (String, Option[String], Option[String]) =
       formatVersion
     )
 
-    if(fmt.getSchema == null)
+    if(fmt.getSchema == null) {
       throw new Exception("Schema not found")
-    if(fmt.getSchema.getPartitionKeyGroup == null)
+    }
+    if(fmt.getSchema.getPartitionKeyGroup == null) {
       throw new Exception("Partition Key Group not found")
+    }
 
     log.info(s"Using PartitionKey ${fmt.getPartitionKey}, PartitionKeyGroup ${fmt.getSchema.getPartitionKeyGroup}")
 
