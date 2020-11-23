919d9a100a163f79055e7188497943fbfcbf989a
Anton Gudin <anton.gudin@finra.org>
2020-11-23 18:35:42 -0500

    DM-13829: Manage Relational Table metadata in Format
    - Process relational schema and table names from format instead of attributes

--- herd-code/herd-service/src/main/java/org/finra/herd/service/helper/BusinessObjectFormatDaoHelper.java
+++ herd-code/herd-service/src/main/java/org/finra/herd/service/helper/BusinessObjectFormatDaoHelper.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2015 herd contributors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 herd contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.finra.herd.service.helper;
 
 import org.apache.commons.lang3.StringUtils;
@@ -43,59 +43,13 @@ public class BusinessObjectFormatDaoHelper
     private RetentionTypeDao retentionTypeDao;
 
     /**
-     * Gets attribute value by name from the business object format entity while specifying whether the attribute is required and whether the attribute value is
-     * required.
-     *
-     * @param attributeName the attribute name (case insensitive)
-     * @param businessObjectFormatEntity the storage entity
-     * @param attributeRequired specifies whether the attribute is mandatory (i.e. whether it has a value or not)
-     * @param attributeValueRequiredIfExists specifies whether the attribute value is mandatory (i.e. the attribute must exist and its value must also contain a
-     * value)
-     *
-     * @return the attribute value from the attribute with the attribute name
-     */
-    public String getBusinessObjectFormatAttributeValueByName(String attributeName, BusinessObjectFormatEntity businessObjectFormatEntity,
-        boolean attributeRequired, boolean attributeValueRequiredIfExists)
-    {
-        boolean attributeExists = false;
-        String attributeValue = null;
-
-        for (BusinessObjectFormatAttributeEntity attributeEntity : businessObjectFormatEntity.getAttributes())
-        {
-            if (attributeEntity.getName().equalsIgnoreCase(attributeName))
-            {
-                attributeExists = true;
-                attributeValue = attributeEntity.getValue();
-                break;
-            }
-        }
-
-        // If the attribute must exist and doesn't, throw an exception.
-        if (attributeRequired && !attributeExists)
-        {
-            throw new IllegalArgumentException(String
-                .format("Attribute \"%s\" for business object format must be configured. Business object format: {%s}", attributeName,
-                    businessObjectFormatHelper.businessObjectFormatEntityAltKeyToString(businessObjectFormatEntity)));
-        }
-
-        // If the attribute is configured, but has a blank value, throw an exception.
-        if (attributeExists && attributeValueRequiredIfExists && StringUtils.isBlank(attributeValue))
-        {
-            throw new IllegalArgumentException(String
-                .format("Attribute \"%s\" for business object format must have a value that is not blank. Business object format: {%s}", attributeName,
-                    businessObjectFormatHelper.businessObjectFormatEntityAltKeyToString(businessObjectFormatEntity)));
-        }
-
-        return attributeValue;
-    }
-
-    /**
      * Gets a business object format entity based on the alternate key and makes sure that it exists. If a format version isn't specified in the business object
      * format alternate key, the latest available format version will be used.
      *
      * @param businessObjectFormatKey the business object format key
      *
      * @return the business object format entity
+     *
      * @throws ObjectNotFoundException if the business object format entity doesn't exist
      */
     public BusinessObjectFormatEntity getBusinessObjectFormatEntity(BusinessObjectFormatKey businessObjectFormatKey) throws ObjectNotFoundException
--- herd-code/herd-service/src/main/java/org/finra/herd/service/impl/BusinessObjectFormatServiceImpl.java
+++ herd-code/herd-service/src/main/java/org/finra/herd/service/impl/BusinessObjectFormatServiceImpl.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2015 herd contributors
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2015 herd contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.finra.herd.service.impl;
 
 import static org.finra.herd.model.dto.SearchIndexUpdateDto.SEARCH_INDEX_UPDATE_TYPE_UPDATE;
@@ -105,8 +105,7 @@ public class BusinessObjectFormatServiceImpl implements BusinessObjectFormatServ
 {
     private static final Logger LOGGER = LoggerFactory.getLogger(BusinessObjectFormatServiceImpl.class);
 
-    private static final String SCHEMA_COLUMN_CSV_INJECTION_ERROR_MSG =
-        "One or more schema column fields start with a prohibited character.";
+    private static final String SCHEMA_COLUMN_CSV_INJECTION_ERROR_MSG = "One or more schema column fields start with a prohibited character.";
 
     /**
      * List all schema column data types for which size increase is considered to be an additive schema change.
@@ -319,10 +318,6 @@ public class BusinessObjectFormatServiceImpl implements BusinessObjectFormatServ
 
         // Validate optional schema information.  This is also going to trim the relative schema column field values.
         validateBusinessObjectFormatSchema(request.getSchema(), businessObjectFormatEntity.getPartitionKey());
-
-        // Update relational table related fields
-        businessObjectFormatEntity.setRelationalSchemaName(request.getRelationalSchemaName());
-        businessObjectFormatEntity.setRelationalTableName(request.getRelationalTableName());
 
         // Update business object format attributes
         updateBusinessObjectFormatAttributesHelper(businessObjectFormatEntity, request.getAttributes());
@@ -858,6 +853,18 @@ public class BusinessObjectFormatServiceImpl implements BusinessObjectFormatServ
 
         // Perform validation of the partition key. This method also trims the partition key value.
         request.setPartitionKey(alternateKeyHelper.validateStringParameter("partition key", request.getPartitionKey()));
+
+        // Trim relational schema name, if specified.
+        if (request.getRelationalSchemaName() != null)
+        {
+            request.setRelationalSchemaName(request.getRelationalSchemaName().trim());
+        }
+
+        // Trim relational table name, if specified.
+        if (request.getRelationalTableName() != null)
+        {
+            request.setRelationalTableName(request.getRelationalTableName().trim());
+        }
 
         // Validate attributes.
         attributeHelper.validateFormatAttributes(request.getAttributes());
