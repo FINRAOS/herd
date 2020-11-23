5a1d3039a34c7868f18a0385c889ee2474e59685
Anton Gudin <anton.gudin@finra.org>
2020-11-23 18:35:42 -0500

    DM-13829: Manage Relational Table metadata in Format
    - Remove relational schema name and table name from PUT request

--- herd-code/herd-service/src/test/java/org/finra/herd/service/impl/BusinessObjectFormatServiceImplTest.java
+++ herd-code/herd-service/src/test/java/org/finra/herd/service/impl/BusinessObjectFormatServiceImplTest.java
@@ -765,8 +765,7 @@ public class BusinessObjectFormatServiceImplTest extends AbstractServiceTest
 
         // Create a business object format.
         businessObjectFormatServiceImpl.updateBusinessObjectFormat(businessObjectFormatKey,
-            new BusinessObjectFormatUpdateRequest(FORMAT_DESCRIPTION, NO_FORMAT_DOCUMENT_SCHEMA, NO_FORMAT_DOCUMENT_SCHEMA_URL, NO_ATTRIBUTES, schema,
-                NO_FORMAT_RELATIONAL_SCHEMA_NAME, NO_FORMAT_RELATIONAL_TABLE_NAME));
+            new BusinessObjectFormatUpdateRequest(FORMAT_DESCRIPTION, NO_FORMAT_DOCUMENT_SCHEMA, NO_FORMAT_DOCUMENT_SCHEMA_URL, NO_ATTRIBUTES, schema));
 
         verify(businessObjectFormatHelper).validateBusinessObjectFormatKey(businessObjectFormatKey);
         verify(attributeHelper).validateFormatAttributes(NO_ATTRIBUTES);
