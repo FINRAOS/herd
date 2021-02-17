8b2359bca0d998a959be1d05f4124bba88ed1718
Zhang, Jim <Jim.Zhang@finra.org>
2021-02-16 21:24:40 -0500

    DM-14307 Herd/Spark - load dataframe with POD data and a large partition range -- clean up

--- herd-code/herd-tools/herd-spark-data-source/src/main/scala/org/apache/spark/sql/herd/HerdFileIndex_Spark_2_2.scala
+++ herd-code/herd-tools/herd-spark-data-source/src/main/scala/org/apache/spark/sql/herd/HerdFileIndex_Spark_2_2.scala
@@ -67,23 +67,6 @@ private[sql] class HerdFileIndex(
 
     }
 
-    /* if (isTraceEnabled()) {
-      val auditInfo = prunedPartitions.map(p =>
-        HerdFileIndexBase.parsePartitionPath(p.path.toString)).zip(selectedPartitions)
-
-      auditInfo.foreach {
-        case (objInfo, dir) =>
-          log.trace(s"INPUT\t${businessObjectName}\t${namespace}\t" +
-            s"${formatUsage}\t${formatFileType}\t" +
-            s"${objInfo("formatVersion").get}\t${objInfo("dataVersion").get}\t" +
-            s"${objInfo("partitionValue").get}\t${objInfo("subPartitionValues").getOrElse("")}\t" +
-            s"${"S3"}\t" +
-            "\t" +
-            s"${dir.files.asInstanceOf[Seq[Any]].map(FileStatusShim.getPath(_).toUri.getPath).mkString(",")}\t" +
-            "AVAILABLE")
-      }
-    } */
-
     selectedPartitions
   }
 
