9ba166bd0c51de6cd8ca9544a0015703d1753de6
Anton Gudin <Anton.Gudin@finra.org>
2021-03-25 17:12:16 -0400

    DM-14457: Spike - Improve performance of BData Post with large number of Storage Files
    - Cleanup usings

--- herd-code/herd-ui/src/test/java/org/finra/herd/ui/RequestLoggingFilterTest.java
+++ herd-code/herd-ui/src/test/java/org/finra/herd/ui/RequestLoggingFilterTest.java
@@ -17,12 +17,8 @@ package org.finra.herd.ui;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 
 import javax.servlet.FilterChain;
@@ -34,7 +30,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
