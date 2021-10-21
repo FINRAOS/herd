f9f894b005671fe342079da6790786918e6c6359
Alex Zheng <wz1371@nyu.edu>
2021-10-21 13:30:50 -0400

    DM-15072: Appcert DATAMGT 2021_Q3 finding- Descriptive Server Signature/ Improper Error Handling - Fix checkstyle issue

--- herd-code/herd-tools/herd-access-validator/src/main/java/org/finra/herd/tools/access/validator/AccessValidatorApp.java
+++ herd-code/herd-tools/herd-access-validator/src/main/java/org/finra/herd/tools/access/validator/AccessValidatorApp.java
@@ -187,7 +187,8 @@ public class AccessValidatorApp
             {
                 BuildInformation buildInformation = applicationContext.getBean(BuildInformation.class);
                 System.out.println(String
-                    .format(ToolsCommonConstants.BUILD_INFO_STRING_FORMAT, buildInformation.getBuildDate(), buildInformation.getBuildNumber(), buildInformation.getBuildUser()));
+                    .format(ToolsCommonConstants.BUILD_INFO_STRING_FORMAT, buildInformation.getBuildDate(),
+                        buildInformation.getBuildNumber(), buildInformation.getBuildUser()));
                 return ToolsCommonConstants.ReturnValue.SUCCESS;
             }
 
